e0ecc4cad58b189d136244b3f00c0f75629fad06
VYOM PATEL <vyom1912@gmail.com>
2026-01-25 16:59:38 +0530

    Initial commit

--- URL-Shortener/app.js
+++ URL-Shortener/app.js
@@ -1,97 +1,21 @@
-import { readFile, writeFile } from "fs/promises";
-import path from "path";
-import crypto from "crypto";
 import express from "express";
+// if use default export router in routes/shortener.routes.js
+// import { Router } from "express";
+// or 
+// import * as shortenerRoutes from "./routes/shortener.routes.js";
+import {shortenerRouter} from "./routes/shortener.routes.js";
 
-const app = express();
+export const app = express();
+
 const PORT = process.env.PORT || 3000;
-const DATA_FILE = path.join("data", "links.json");
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
-
-const loadLinks = async () => {
-  try {
-    const data = await readFile(DATA_FILE, "utf-8");
-    return JSON.parse(data || "{}");
-  } catch (error) {
-    if (error.code === "ENOENT") {
-      await writeFile(DATA_FILE, JSON.stringify({}));
-      return {};
-    }
-    throw error;
-  }
-};
-
-const saveLinks = async (links) => {
-  await writeFile(DATA_FILE, JSON.stringify(links), "utf-8");
-};
-
-app.get("/", async (req, res) => {
-  try {
-    const file = await readFile(path.join("views", "index.html"));
-    const links = await loadLinks();
-
-    const list = Object.entries(links)
-      .map(
-        ([shortCode, url]) =>
-          `<li><a href="/${shortCode}" target="_blank">${req.host}</a> - ${url}</li>`,
-      )
-      .join("");
-    res.send(file.toString().replaceAll("{{shortened_urls}}", list));
-    // ----------- OR ----------- for replacing in  url list dynamically
-    // const content = file.toString().replaceAll(
-    //   "{{shortened_urls}}",
-    //   Object.entries(links)
-    //     .map(
-    //       ([shortCode, url]) =>
-    //         `<li><a href="/${shortCode}" target="_blank">${req.host}</a> - ${url}</li>`,
-    //     )
-    //     .join(""),
-    // );
-    // return res.send(content);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).send(" get Internal Server Error");
-  }
-});
-
-app.post("/", async (req, res) => {
-  try {
-    const { url, shortCode } = req.body;
-    const finalShortCode = shortCode || crypto.randomBytes(4).toString("hex");
-
-    const links = await loadLinks();
-
-    if (links[finalShortCode]) {
-      return res
-        .status(400)
-        .send("Short code already exists. Please choose another.");
-    }
-    links[finalShortCode] = url;
-    await saveLinks(links);
-    return res.redirect("/");
-  } catch (error) {
-    console.log(error);
-    return res.status(500).send("post Internal Server Error");
-  }
-});
-
-app.post("/:shorten", async (req, res) => {
-  try {
-    const { shortCode } = req.params;
-    const links = await loadLinks();
-    if (!links[shortCode]) {
-      return res.status(404).send("Short URL not found");
-    }
-
-    return res.redirect(links[shortCode]);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).send("Internal Server Error");
-  }
-});
-
+// express app use the router
+// if use default export router in routes/shortener.routes.js
+// app.use("/", router);
+// or
+app.use("/", shortenerRouter);
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
---
+++ URL-Shortener/routes/shortener.routes.js
@@ -0,0 +1,99 @@
+import { readFile, writeFile } from "fs/promises";
+import crypto from "crypto";
+import path from "path";
+import { Router } from "express";
+
+const router=Router();
+
+const DATA_FILE = path.join("data", "links.json");
+
+
+const loadLinks = async () => {
+  try {
+    const data = await readFile(DATA_FILE, "utf-8");
+    return JSON.parse(data || "{}");
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      await writeFile(DATA_FILE, JSON.stringify({}));
+      return {};
+    }
+    throw error;
+  }
+};
+
+const saveLinks = async (links) => {
+  await writeFile(DATA_FILE, JSON.stringify(links), "utf-8");
+};
+
+router.get("/", async (req, res) => {
+  try {
+    const file = await readFile(path.join("views", "index.html"));
+    const links = await loadLinks();
+
+    const list = Object.entries(links)
+      .map(
+        ([shortCode, url]) =>
+          `<li><a href="/${shortCode}" target="_blank">${req.host}</a> - ${url}</li>`,
+      )
+      .join("");
+    res.send(file.toString().replaceAll("{{shortened_urls}}", list));
+    // ----------- OR ----------- for replacing in  url list dynamically
+    // const content = file.toString().replaceAll(
+    //   "{{shortened_urls}}",
+    //   Object.entries(links)
+    //     .map(
+    //       ([shortCode, url]) =>
+    //         `<li><a href="/${shortCode}" target="_blank">${req.host}</a> - ${url}</li>`,
+    //     )
+    //     .join(""),
+    // );
+    // return res.send(content);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(" get Internal Server Error");
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const { url, shortCode } = req.body;
+    const finalShortCode = shortCode || crypto.randomBytes(4).toString("hex");
+
+    const links = await loadLinks();
+
+    if (links[finalShortCode]) {
+      return res
+        .status(400)
+        .send("Short code already exists. Please choose another.");
+    }
+    links[finalShortCode] = url;
+    await saveLinks(links);
+    return res.redirect("/");
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("post Internal Server Error");
+  }
+});
+
+router.post("/:shorten", async (req, res) => {
+  try {
+    const { shortCode } = req.params;
+    const links = await loadLinks();
+    if (!links[shortCode]) {
+      return res.status(404).send("Short URL not found");
+    }
+
+    return res.redirect(links[shortCode]);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Internal Server Error");
+  }
+});
+// small project
+// export default router;
+
+// Name export for bigger project
+// const shortenerRouter=router;
+// export default shortenerRouter;
+// or
+export const shortenerRouter=router;
